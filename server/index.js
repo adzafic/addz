68ddd2974dfc8ae1f38a2dc3bb70d3e970eb0e6b
Adnan Dzafic Dzafic <adzafic@MacBook-Pro-de-Adnan.local>
2020-12-08 16:18:07 +0100

    añadir mongoose

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 3000;
+// conectar a la base de datps
+connectDB();
 //trasformar peticiones
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,6 +16,5 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send({ message: 'Hola mundo' });
 });
-app.listen(port, () => {
-  console.log(`Servidor lanzado en puerto: ${port}`);
-});
+
+app.listen(port, () => console.log(`Servidor lanzado en puerto: ${port}`));
