31e029c365f9cee1670521ddb0d284873334d9af
Adnan Dzafic Dzafic <adzafic@MacBook-Pro-de-Adnan.local>
2020-12-08 11:25:03 +0100

    add start al script y ruta get

--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
+//trasformar peticiones
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//endpoints
+app.get('/', (req, res) => {
+  res.send({ message: 'Hola mundo' });
+});
 app.listen(port, () => {
   console.log(`Servidor lanzado en puerto: ${port}`);
 });
