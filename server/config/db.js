ead013d4a64177e982b4598c72468af5b60d1086
Adnan Dzafic Dzafic <adzafic@MacBook-Pro-de-Adnan.local>
2020-12-09 20:54:11 +0100

    Registrar usuario

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log('mongo db conected');
   } catch (err) {
     console.log(err.message);
--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 //@route POST api/users
 //@desc test users
@@ -15,18 +17,34 @@ router.post(
       'La contraseña tiene que tener mas de 6 caracteres'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const error = validationResult(req);
     if (!error.isEmpty()) {
       return res.status(400).json({ errors: error.array() });
     }
 
-    //buscar usuario
+    const { name, email, password } = req.body;
 
-    //encriptar contraseña
-    res.send({
-      message: 'user route',
-    });
+    try {
+      let user = await User.findOne({ email });
+      console.log(user, name, email, password);
+      if (user) {
+        res.status(400).send({ errors: [{ msg: 'Usuario ya existe' }] });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      console.log(user);
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.status(200).json({ msg: 'Usuario registrado' });
+    } catch (error) {
+      res.status(500).json({ errors: [{ msg: error }] });
+    }
   }
 );
 
