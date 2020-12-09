94ced9ddd6f5225512e81d11acb395689e0374d3
Adnan Dzafic Dzafic <adzafic@MacBook-Pro-de-Adnan.local>
2020-12-09 21:49:52 +0100

    crear jwtoken

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //@route POST api/users
 //@desc test users
@@ -41,7 +43,21 @@ router.post(
 
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      res.status(200).json({ msg: 'Usuario registrado' });
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({ token });
+        }
+      );
+      //res.status(200).json({ msg: 'Usuario registrado' });
     } catch (error) {
       res.status(500).json({ errors: [{ msg: error }] });
     }
