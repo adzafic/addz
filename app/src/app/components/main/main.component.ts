68d1ca052c65bcdd0e3b8716a570deddfbb3248a
Adnan Dzafic Dzafic <adzafic@MacBook-Pro-de-Adnan.local>
2020-12-25 21:32:32 +0100

    añadir link a raiz e input file

--- app/src/app/components/main/main.component.ts
+++ app/src/app/components/main/main.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
   click() {
-    console.log('click');
+    this.router.navigateByUrl('/add-client');
   }
 }
